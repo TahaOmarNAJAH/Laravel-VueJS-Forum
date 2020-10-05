bee10fba3fcf5d20f6ee72f3c5b5a88bcd904347
Taha Omar Najah <tahaomarnajah@gmail.com>
2020-10-05 19:07:29 +0100

    integrated JWT

--- app/User.php
+++ app/User.php
@@ -7,8 +7,9 @@ use App\Models\Reply;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Tymon\JWTAuth\Contracts\JWTSubject;
 
-class User extends Authenticatable
+class User extends Authenticatable implements JWTSubject
 {
     use Notifiable;
 
@@ -44,6 +45,25 @@ class User extends Authenticatable
     {
         return $this->hasMany(Question::class);
     }
+    // Rest omitted for brevity
 
+    /**
+     * Get the identifier that will be stored in the subject claim of the JWT.
+     *
+     * @return mixed
+     */
+    public function getJWTIdentifier()
+    {
+        return $this->getKey();
+    }
 
+    /**
+     * Return a key value array, containing any custom claims to be added to the JWT.
+     *
+     * @return array
+     */
+    public function getJWTCustomClaims()
+    {
+        return [];
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -21,5 +21,18 @@ use Illuminate\Support\Facades\Route;
 Route::apiResource('/question', 'QuestionController');
 Route::apiResource('/category', 'CategoryController');
 Route::apiResource('question/{question}/reply', 'replyController');
-Route::post('like/{reply}','LikeController@likeIt');
-Route::delete('like/{reply}','LikeController@unlikeIt');
+Route::post('like/{reply}', 'LikeController@likeIt');
+Route::delete('like/{reply}', 'LikeController@unlikeIt');
+
+Route::group([
+
+    'middleware' => 'api',
+    'prefix' => 'auth'
+
+], function ($router) {
+
+    Route::post('login', 'AuthController@login');
+    Route::post('logout', 'AuthController@logout');
+    Route::post('refresh', 'AuthController@refresh');
+    Route::post('me', 'AuthController@me');
+});
